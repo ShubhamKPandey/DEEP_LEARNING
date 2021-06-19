ddc59e66846a68da244e57cc34b1039fe47c2e2b
SKP <divyam.panday@gmail.com>
2021-06-19 18:59:46 +0530

    Dense

--- RNN.py
+++ RNN.py
@@ -1,9 +1,10 @@
 import numpy as np
 from numpy import random
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 def generate_time_series(batch_size = 1 , n_time_steps = 50, n = 1):
-    freq1, freq2, offset1 , offset2 = random.randn(4, batch_size, 1)
+    freq1, freq2, offset1 , offset2 = random.rand(4, batch_size, 1)
     timesteps = np.linspace(0.0, 1.0, num = n_time_steps)
 
     series = 0.5*np.sin(
@@ -18,13 +19,24 @@ def generate_time_series(batch_size = 1 , n_time_steps = 50, n = 1):
     return series.astype(np.float32)
 
 
-
-
-
-steps = 200
+steps = 50
 batch_size = 10000
 dataset = np.array(generate_time_series(batch_size,n_time_steps = steps + 1))
 
-X_train, y_train = dataset[0:7000,0:steps], dataset[0:7000, steps]
-X_valid, y_valid = dataset[7000:9000, 0:steps], dataset[7000:9000, steps]
-X_test, y_test = dataset[9000:, 0:steps], dataset[9000:, steps]
+X_train, y_train = dataset[0:7000,0:steps], dataset[0:7000, -1]
+X_valid, y_valid = dataset[7000:9000, 0:steps], dataset[7000:9000, -1]
+X_test, y_test = dataset[9000:, 0:steps], dataset[9000:, -1]
+print(y_valid.shape, y_train.shape)
+
+input = tf.keras.layers.Input(shape = (steps,1))
+x = tf.keras.layers.Flatten()(input)
+y = tf.keras.layers.Dense(1)(x)
+
+
+model = tf.keras.Model(inputs = [input], outputs = [y])
+model.compile(loss = "mean_squared_error", optimizer = "Adam")
+history = model.fit(X_train, y_train,epochs = 20,validation_data=(X_valid, y_valid))
+
+
+
+
