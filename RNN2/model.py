1f7034dbbfc4e910ca4ab9200306cc03b062cda5
SKP <divyam.panday@gmail.com>
2021-06-22 12:49:39 +0530

    RNN

--- RNN2/model.py
+++ RNN2/model.py
@@ -7,3 +7,18 @@ import pandas as pd
 
 gpu = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpu[0], True)
+
+shakespeare_url = "https://homl.info/shakespeare" # shortcut URL
+filepath = keras.utils.get_file("shakespeare.txt", shakespeare_url)
+with open(filepath) as f:
+    shakespeare_text = f.read()
+
+tokenizer = keras.preprocessing.text.Tokenizer(char_level = True)
+tokenizer.fit_on_texts([shakespeare_text])
+print(tokenizer.document_count)
+
+print(np.array(tokenizer.texts_to_sequences([shakespeare_text])))
+print(np.array(tokenizer.sequences_to_texts([[34, 5, 6, 23, 7]])))
+
+[encoded] = np.array(tokenizer.texts_to_sequences([shakespeare_text])) - 1
+print(encoded)
