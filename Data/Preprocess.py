9cdcc62ef2280a76536326fd62dd938e45fe58c5
SKP <divyam.panday@gmail.com>
2021-06-26 19:12:21 +0530

    RNN

--- Data/Preprocess.py
+++ Data/Preprocess.py
@@ -19,3 +19,29 @@ indices = tf.range(len(vocab), dtype = tf.int64)
 table_init = tf.lookup.KeyValueTensorInitializer(vocab, indices)
 num_oov_buckets = 2
 table = tf.lookup.StaticVocabularyTable(table_init, num_oov_buckets)
+
+
+categories = tf.constant(["NEAR BAY", "DESERT", "INLAND", "INLAND"])
+cat_indices = table.lookup(categories)
+print(cat_indices)
+cat_one_hot = tf.one_hot(cat_indices, depth = len(vocab) + num_oov_buckets)
+print(cat_one_hot)
+
+embedding_dim  = 2
+embed_init  = tf.random.uniform([len(vocab) + num_oov_buckets, embedding_dim])
+embedding_matrix = tf.Variable(embed_init)
+print(embedding_matrix)
+
+categories = tf.constant(["NEAR BAY", "DESERT", "INLAND", "INLAND"])
+cat_indices = table.lookup(categories)
+print(cat_indices)
+print(tf.nn.embedding_lookup(embedding_matrix , cat_indices))
+
+regular_inputs = keras.layers.Input(shape = [8])
+categories = keras.layers.Input(shape = [], dtype = tf.string)
+cat_indices = keras.layers.Lambda(lambda cats: table.lookup(cats))(categories)
+cat_embed = keras.layers.Embedding(input_dim = 6, output_dim = 2)(cat_indices)
+encoded_inputs = keras.layers.concatenate([regular_inputs, cat_embed])
+outputs = keras.layers.Dense(1)(encoded_inputs)
+model = keras.models.Model(inputs = [regular_inputs, categories], outputs = [outputs])
+
--- RNN2/model.py
+++ RNN2/model.py
@@ -33,6 +33,75 @@ n_steps = 100
 window_length  = n_steps + 1
 dataset = dataset.window(window_length, shift = 1, drop_remainder = True )
 
+dataset = dataset.flat_map(lambda window: window.batch(window_length))
+
+batch_size = 32
+dataset = dataset.shuffle(10000).batch(batch_size)
+dataset = dataset.map(lambda windows: (windows[:, :-1], windows[:, 1:]))
+
+dataset = dataset.map(
+    lambda X_batch, Y_batch: (tf.one_hot(X_batch, depth = max_id), Y_batch))
+
+dataset = dataset.prefetch(1)
+
+model =  keras.models.Sequential([
+    keras.layers.GRU(128, return_sequences =True, input_shape = [None, max_id],
+    dropout  =0.2, recurrent_dropout = 0.2
+    ),
+    keras.layers.GRU(128, return_sequences = True,
+    dropout = 0.2, recurrent_dropout = 0.2),
+    keras.layers.TimeDistributed(keras.layers.Dense(max_id, 
+    activation = "softmax"))
+]
+)
+
+model.compile(loss = "sparse_categorical_crossentropy", optimizer = "adam")
+
+def preprocess(texts):
+    X = np.array(tokenizer.texts_to_sequences(texts)) - 1
+    return tf.one_hot(X, max_id)
+
+def next_char(text, temperature = 1):
+    X_new = preprocess([text])
+    y_proba = model.predict(X_new)[0,-1:,: ]
+    rescaled_logits = tf.math.log(y_proba) / temperature
+    char_id = tf.random.categorical(rescaled_logits, num_samples =1) + 1
+    return tokenizer.sequences_to_texts(char_id.numpy())[0]
+
+def complete_text(text, n_chars = 50, temperature = 1):
+    for _ in range(n_chars):
+        text += next_char(text, temperature)
+    return text
 
 
+#Stateful
+dataset = tf.data.Dataset.from_tensor_slices(encoded[:train_size])
+dataset = dataset.window(window_length, shift = n_steps, drop_remainder = True)
+dataset = dataset.flat_map(lambda window: window.batch(window_length))
+dataset = dataset.batch(1)
+dataset = dataset.map(lambda windows: (windows[:, :-1], windows[:,1:]))
+dataset = dataset.map(
+    lambda X_batch, Y_batch: (tf.one_hot(X_batch, depth = max_id), Y_batch)
+)
+dataset = dataset.prefetch(1)
 
+tf.train.Dataset.zip(dataset).map(lambda *windows:
+tf.stack(windows))
+
+
+model = keras.models.Sequential([
+keras.layers.GRU(128, return_sequences=True, stateful=True,
+dropout=0.2, recurrent_dropout=0.2,
+batch_input_shape=[batch_size, None, max_id]),
+keras.layers.GRU(128, return_sequences=True, stateful=True,
+dropout=0.2, recurrent_dropout=0.2),
+keras.layers.TimeDistributed(keras.layers.Dense(max_id,
+activation="softmax"))
+])
+
+class ResetStatesCallback(keras.callbacks.Callback):
+    def on_epoch_begin(self, epoch, logs):
+        self.model.reset_states()
+
+model.compile(loss="sparse_categorical_crossentropy", optimizer="adam")
+model.fit(dataset, epochs=50, callbacks=[ResetStatesCallback()])
